442e57e839ce3504c87b36cd69835abcd19d237f
Tejas Jadhav <tjadhav9@gmail.com>
2018-12-24 18:51:38 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,10 +1,11 @@
 
 import os
+import shutil
 
 os.getcwd()
 collection = "DataSet"
-training_folder = "Data\Training"
-testing_folder = "Data\Testing"
+training_folder = "Data/Training"
+testing_folder = "Data/Testing"
 
 images=0
 classes=0
@@ -17,10 +18,20 @@ f = open("datasetCount.txt", "a")
 for folder in os.listdir(collection):
     classes+=1
     j=0
+    list = os.listdir(collection+"/"+folder)
+    total = len(list)
     for file in os.listdir(collection+"/"+folder):
         j+=1
         images+=1
-        #os.rename("DataSet/"+folder+"/"+file,"DataSet/"+folder+"/"+folder+"_"+str(j)+".png")
+        exists = os.path.isfile("DataSet/"+folder+"/"+folder+"_"+str(j)+".png")
+        if not exists:
+            os.rename("DataSet/" + folder + "/" + file, "DataSet/" + folder + "/" + folder + "_" + str(j) + ".png")
+
+        dataFolder = training_folder if (j < (4*total/5)) else testing_folder
+        dataexists = os.path.isfile(dataFolder + "/" + folder + "_" + str(j) + ".png")
+        if not dataexists:
+            shutil.copy("DataSet/" + folder + "/" + folder + "_" + str(j) + ".png", dataFolder)
+
     print(folder +"\t"+ str(j)+ " images")
     f.write(folder +"\t"+ str(j)+ " images\n")
 
@@ -31,11 +42,17 @@ f.write("\n"+str(authors)+" Authors, "+str(classes)+" Classes, "+str(images)+" I
 training_files = 0
 for file in os.listdir(training_folder):
     training_files += 1
-
+training_files_percent = round((100*training_files/images),2)
 
 testing_files = 0
 for file in os.listdir(testing_folder):
     testing_files += 1
-print ("\n"+str(training_files)+" Training images, "+str(testing_files)+" Testing images")
-f.write("\n"+str(training_files)+" Training images, "+str(testing_files)+" Testing images")
+testing_files_percent = round((100*testing_files/images),2)
+
+print ("\n"+str(training_files)+" Training images ("+str(training_files_percent)+"%)")
+print ("\n"+str(testing_files)+" Testing images ("+str(testing_files_percent)+"%)")
+f.write("\n"+str(training_files)+" Training images ("+str(training_files_percent)+"%)")
+f.write("\n"+str(testing_files)+" Testing images ("+str(testing_files_percent)+"%)")
+
+
 f.close()
